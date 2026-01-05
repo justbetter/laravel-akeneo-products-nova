5b78510c0348892a46d669a4d461eac1423e4c95
Vincent Boon <vincent@justbetter.nl>
2026-01-05 14:41:10 +0100

    Support multiple identifiers

--- src/Nova/Actions/Product/RetrieveByIdentifier.php
+++ src/Nova/Actions/Product/RetrieveByIdentifier.php
@@ -7,6 +7,7 @@ use JustBetter\AkeneoProducts\Jobs\Product\RetrieveProductJob;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Actions\ActionResponse;
 use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
@@ -21,6 +22,19 @@ class RetrieveByIdentifier extends Action
     public function handle(ActionFields $fields, Collection $models): ActionResponse
     {
         $identifier = $fields->get('identifier');
+        $csv = $fields->get('csv', false);
+
+        if ($csv) {
+            $identifiers = array_map('trim', explode(',', $identifier));
+
+            foreach ($identifiers as $identifier) {
+                if (! empty($identifier)) {
+                    RetrieveProductJob::dispatch($identifier);
+                }
+            }
+
+            return ActionResponse::message(__('Retrieving :count products', ['count' => count($identifiers)]));
+        }
 
         RetrieveProductJob::dispatch($identifier);
 
@@ -32,6 +46,9 @@ class RetrieveByIdentifier extends Action
         return [
             Text::make(__('Identifier'), 'identifier')
                 ->required(),
+
+            Boolean::make(__('CSV'), 'csv')
+                ->help(__('Check this if you are providing a comma-separated list of identifiers. i.e.: identifier1,identifier2')),
         ];
     }
 }
